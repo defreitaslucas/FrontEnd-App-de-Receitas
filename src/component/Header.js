3ce7257416f0df14a51c3745e1d1f2b6f1492c0f
SergioEvando23 <sergioevandocosta@gmail.com>
2022-03-29 13:40:14 -0300

    Adicionado redirecionamento aos botões do header, e open/close da barra de pesquisas

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,22 +1,45 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import searchIcon from '../images/searchIcon.svg';
 import profileIcon from '../images/profileIcon.svg';
 
 function Header({ title }) {
+  const [searchBar, setSearchBar] = useState(false);
+
+  const handleClick = () => {
+    if (searchBar === false) {
+      setSearchBar(true);
+    } else if (searchBar === true) {
+      setSearchBar(false);
+    }
+  };
+
   return (
     <section className="header-component">
-      <button type="button" data-testid="profile-top-btn">
-        <img src={ profileIcon } alt="icone de perfil" />
-      </button>
+      <Link to="/profile">
+        <img
+          data-testid="profile-top-btn"
+          src={ profileIcon }
+          alt="icone de perfil"
+        />
+      </Link>
       <h2 data-testid="page-title">{title}</h2>
       { title === 'foods' || title === 'explore nationalities'
         ? (
-          <button type="button" data-testid="search-top-btn">
-            <img src={ searchIcon } alt="lupa de procura" />
+          <button type="button" onClick={ handleClick }>
+            <img
+              data-testid="search-top-btn"
+              src={ searchIcon }
+              alt="lupa de procura"
+            />
           </button>
         )
         : null }
+
+      {searchBar
+        ? <input type="text" placeholder="search" />
+        : null}
     </section>
   );
 }
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from '../component/Header';
 
 function Login() {
   return (
     <div>
+      <Header title="foods" />
       Login
     </div>
   );
