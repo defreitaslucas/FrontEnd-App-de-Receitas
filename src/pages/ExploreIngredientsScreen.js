2ecac7f73baa006f2d66cfc5f47408acd3585d31
C.Swame <caio_swame@hotmail.com>
2022-03-30 14:55:30 -0300

    requisitos 75 e 76 - criacao da lista de ingredientes a ser explorada

--- src/pages/ExploreIngredientsScreen.js
+++ src/pages/ExploreIngredientsScreen.js
@@ -1,7 +1,79 @@
-import React from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { fetchCocktailDetails } from '../services/fetchCocktail';
+import { fetchMealsDetails } from '../services/fetchMealsDetails';
+import './ExploreIngredientsScreen.css';
 
-export default function ExploreIngredientsScreen() {
+const MAGIC_NUMBER_TWELVE = 12;
+
+export default function ExploreIngredientsScreen({ location }) {
+  const isFood = location.pathname.includes('foods');
+  const isFoodStr = isFood ? 'foods' : 'drinks';
+  const [ingredients, setIngredients] = useState([]);
+
+  const setInitialData = useCallback(async () => {
+    const listIngredientsData = isFood
+      ? await fetchMealsDetails('i')
+      : await fetchCocktailDetails('i');
+    const listIngredients = isFood
+      ? listIngredientsData.meals
+      : listIngredientsData.drinks;
+    setIngredients(listIngredients);
+  }, [isFood]);
+
+  useEffect(() => {
+    setInitialData();
+  }, [setInitialData]);
+
   return (
-    <div>ExploreIngredientsScreen</div>
+    <main>
+      <header>
+        <h1>
+          { isFoodStr.toUpperCase() }
+          {' '}
+          INGREDIENTS
+        </h1>
+      </header>
+      <section>
+        {ingredients
+          .map((ingredientData, index) => {
+            if (index < MAGIC_NUMBER_TWELVE) {
+              const { idIngredient, strDescription, strIngredient } = ingredientData;
+              const { strIngredient1 } = ingredientData;
+              const img = isFood
+                ? `https://www.themealdb.com/images/ingredients/${strIngredient}-Small.png`
+                : `https://www.thecocktaildb.com/images/ingredients/${strIngredient1}-Small.png`;
+              return (
+                <div
+                  key={ isFood ? idIngredient : strIngredient1 }
+                  data-testid={ `${index}-ingredient-card` }
+                >
+                  <img
+                    data-testid={ `${index}-card-img` }
+                    src={ img }
+                    alt={ `${strIngredient}; click to see recipes with this ingredient` }
+                  />
+                  <h4 data-testid={ `${index}-card-name` }>
+                    {strIngredient || strIngredient1}
+                  </h4>
+                  <p className="ingredient-description">
+                    {strDescription}
+                  </p>
+                </div>
+              );
+            }
+            return null;
+          })}
+      </section>
+    </main>
   );
 }
+
+ExploreIngredientsScreen.propTypes = {
+  location: PropTypes.shape({
+    pathname: PropTypes.string.isRequired,
+  }).isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
