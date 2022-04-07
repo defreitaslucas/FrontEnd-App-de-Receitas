db0e62a422c6d43b650a0b8dd60399e57ad9fbae
Rhenan Stocco Zimmermann <rhenansz@hotmail.com>
2022-04-07 01:31:17 -0300

    HeaderSearchBar Test  parcial

--- src/test/HeaderSearchBar.test.js
+++ src/test/HeaderSearchBar.test.js
@@ -8,9 +8,12 @@ import App from '../App';
 // const soupMeals = require('../../cypress/mocks/soupMeals');
 // const ginDrinks = require('../../cypress/mocks/ginDrinks');
 const chickenMeals = require('../../cypress/mocks/chickenMeals');
+const oneMeal = require('../../cypress/mocks/oneMeal');
 
 beforeEach(() => {
-  renderWithRouter(<App />);
+  const { history } = renderWithRouter(<App />);
+  history.push('/foods');
+  /* renderWithRouter(<App />);
   const email = 'test.trybe@test.com';
   const password = '123456789';
   const emailInput = screen.getByTestId('email-input');
@@ -18,7 +21,7 @@ beforeEach(() => {
   const submitButton = screen.getByTestId('login-submit-btn');
   userEvent.type(emailInput, email);
   userEvent.type(passwordInput, password);
-  userEvent.click(submitButton);
+  userEvent.click(submitButton); */
   const searchTopBtn = screen.getByTestId('search-top-btn');
   userEvent.click(searchTopBtn);
 });
@@ -38,18 +41,35 @@ describe('13 - Testing the Search Bar elements', () => {
 });
 
 describe('14 - Testing the radio buttons', () => {
-  it('Testing the Ingredient radio button', () => {
+  it('Testing the Ingredient radio button', async () => {
     jest.spyOn(global, 'fetch');
-    global.fetch.mockReturnValue({
-      json: jest.fn().mockReturnValue(chickenMeals),
+    global.fetch.mockResolvedValue({
+      json: jest.fn().mockResolvedValue(chickenMeals),
     });
-    global.fetch('https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken');
     const ingredientSearchRadio = screen.getByTestId('ingredient-search-radio');
     const searchInput = screen.getByTestId('search-input');
     const execSearchBtn = screen.getByTestId('exec-search-btn');
     userEvent.click(ingredientSearchRadio);
     userEvent.type(searchInput, 'chicken');
     userEvent.click(execSearchBtn);
+    const renderedInfo = await screen.findAllByText(/chicken/i);
+    expect(renderedInfo[0]).toBeInTheDocument();
     expect(global.fetch).toHaveBeenCalledWith('https://www.themealdb.com/api/json/v1/1/filter.php?i=chicken');
+  });
+
+  it('Testing the Name radio button', async () => {
+    jest.spyOn(global, 'fetch');
+    global.fetch.mockResolvedValue({
+      json: jest.fn().mockResolvedValue(oneMeal),
+    });
+    const nameSearchRadio = screen.getByTestId('name-search-radio');
+    const searchInput = screen.getByTestId('search-input');
+    const execSearchBtn = screen.getByTestId('exec-search-btn');
+    userEvent.click(nameSearchRadio);
+    userEvent.type(searchInput, 'soup');
+    userEvent.click(execSearchBtn);
+    const renderedInfo = await screen.findAllByText(/soup/i);
+    expect(renderedInfo[0]).toBeInTheDocument();
+    expect(global.fetch).toHaveBeenCalledWith('https://www.themealdb.com/api/json/v1/1/search.php?s=soup');
   });
 });
